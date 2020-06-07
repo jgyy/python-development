799f94120276f9272507d359c83e01574c4fd60d
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-07 14:44:28 +0800

    tuples and ranges

---
+++ data_types/tuple_range.py
@@ -0,0 +1,15 @@
+# Tuples
+point = (2.0, 3.0)
+point_3d = point + (4.0,)
+print(point_3d)
+x, y, z = point_3d
+print(x)
+print(y)
+print(z)
+print("My name is: %s %s" % ("J", "G"))
+
+# Ranges
+my_range = range(10)
+print(my_range)
+print(list(my_range))
+print(list(range(1, 14, 2)))
