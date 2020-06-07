711950b7eb2e92d3e0f4b3ca3394ad4968a4414a
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-07 14:18:26 +0800

    numbers practice on python

---
+++ data_types/numbers.py
@@ -0,0 +1,17 @@
+# Numbers
+print(2 + 2) # Addition
+print(10 - 4) # Subtraction
+print(3 * 9) # Multiplication
+print(5 / 3) # Division
+print(5 // 3) # Floor division, always returns a number without a remainder
+print(8 % 3) # Modulo division, returns the remainder
+print(2 ** 3) # Exponent
+
+# Converting Strings and Numbers
+print(str(1.1))
+print(int("10"))
+print(int(5.99999))
+print(float("5.6"))
+print(float(5))
+
+# float("1.1 things") # Error
