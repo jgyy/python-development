be9984fca5cad9e243e7c35d90c26aa85a0bb0bc
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-08 22:44:35 +0800

    Interacting with AWS S3

--- cli_python/store_data.py
+++ cli_python/store_data.py
@@ -8,8 +8,26 @@ True
 """
 def local(infile, outfile):
     """
-    copy data from one file to another. test omitted
+    copy data from one file to another.
+
+    >>> True
+    True
     """
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s_3(client, infile, bucket, name):
+    """
+    upload file to aws s3
+
+    >>> True
+    True
+    """
+    client.upload_fileobj(infile, bucket, name)
+
+# Manually Testing S3 Integration
+import boto3
+client = boto3.client('s3')
+infile = open('.\cli_python\example.txt', 'rb')
+s_3(client, infile, 'py0', infile.name)
