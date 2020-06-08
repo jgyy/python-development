e7d1d3531959ba364cdb112daa29b387507e409c
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-08 21:41:31 +0800

    Interacting with External Processes: Utilizing pg_dump

---
+++ cli_python/psql_dump.py
@@ -0,0 +1,23 @@
+"""
+python -m doctest -v .\cli_python\psql_dump.py
+Interacting with Subprocesses
+
+>>> import sys
+>>> sys.version > "3.5"
+True
+"""
+import sys
+import subprocess
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
+
+# Manual Testing
+dump = dump('postgres://jgyy:jgyy@13.229.82.53:80/sample')
+f = open('dump.sql', 'w+b')
+f.write(dump.stdout.read())
+f.close()
