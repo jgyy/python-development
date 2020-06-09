4075eb9db1fd7c4046d92c5f50350b3fe2210506
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-09 17:17:25 +0800

    Modeling Data with an Object-Relational Mapper

--- web_dev/notes/__init__.py
+++ web_dev/notes/__init__.py
@@ -6,7 +6,9 @@ flask run --host=0.0.0.0 --port=3000
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
+# Registering our Database with Our Application
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
@@ -17,5 +19,10 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    from .models import db
+
+    db.init_app(app)
+    Migrate(app, db)
 
     return app
