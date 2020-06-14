6a2b0c194d5bf93961768c8bd5bc3e1f20d99f5d
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-14 19:54:30 +0800

    edited the model class list

--- web_dev/notes/models.py
+++ web_dev/notes/models.py
@@ -12,13 +12,6 @@ class User(db.Model):
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     notes = db.relationship('Note', backref='author', lazy=True)
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    body = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
